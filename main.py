26251dfa26c333943c4f85fa1f4ef36a8a305a4f
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-12-05 13:16:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import openai
 
 # Define the server URL
-public_url = "https://easy-donuts-drum.loca.lt"  
+public_url = "https://common-taxis-refuse.loca.lt"  
 
 # OpenAI API Key
 openai.api_key = st.secrets["mykey"]
