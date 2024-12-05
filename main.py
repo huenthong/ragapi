66552c3aca4dd8de5582b06099ace1ab664cbef3
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-12-05 13:24:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import openai
 
 # Define the server URL
-public_url = "https://common-taxis-refuse.loca.lt"  
+public_url = "https://thick-sides-prove.loca.lt"  
 
 # OpenAI API Key
 openai.api_key = st.secrets["mykey"]
