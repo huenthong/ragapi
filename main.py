1de169c02c49b8b0b0c57d36af6bce851a8e6876
Huenthong_ <126305082+huenthong@users.noreply.github.com>
2024-12-12 02:12:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 import time
 
 # Define the server URL
-public_url = "https://sweet-wolves-train.loca.lt"
+public_url = "https://old-planets-roll.loca.lt"
 
 # OpenAI API Key
 openai.api_key = st.secrets["mykey"]
